4abbb7bb8dfd8349dfba4a6237b6970a64feae23
Ajeet N <ajeetn@ajeets-mac-mini.local>
2016-06-08 16:36:04 +0530

    Initial Commit

---
+++ switch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  switch
+//
+//  Created by Ajeet N on 08/06/16.
+//  Copyright © 2016 AhexTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ switch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  switch
+//
+//  Created by Ajeet N on 08/06/16.
+//  Copyright © 2016 AhexTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
